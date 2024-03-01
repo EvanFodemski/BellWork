d94440006637f79a997862c122d3bd19012e81fc
Evan F <evanfd05@gmail.com>
2024-02-29 18:11:56 -0700

    Worked on homepage and started backend

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -2,13 +2,30 @@ import { useQuery, useMutation } from "@apollo/client";
 
 
 function Home() {
+    
     return (
-        <div>
-            <div className="home1"></div>
-            <div className="home2"></div>
+        <div className="home-container">
+            <h1 className="whatisbell">What is BellWork?</h1>
+            <div className="homeex">
+                Bell Work is a one of a kind workout program builder.
+            </div>
+            <div className="learnmore">Learn More</div>
+            <a href="#bottomhome" className="startbuild">Build a Program for Free</a>
+            <div className="orange-line"></div>
+            <div className="homefill"></div>
+            <img className="thorhome" src="/thornew.jpg" alt="" />
+            
+                <div id="bottomhome" className="homelowermain">
+
+                    <div className="box-container">
+                        <div className="box"></div>
+                        <div className="box"></div>
+                        <div className="box"></div>
+                    </div>
+                </div>
+
+           
         </div>
-
-
     )
 }
 
