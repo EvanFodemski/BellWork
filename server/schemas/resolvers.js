46673876308d556a428fe36b4a5623fe127ead4f
Evan F <evanfd05@gmail.com>
2024-05-08 19:04:43 -0600

    Finally got the days to schedule mutation working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -102,8 +102,6 @@ const resolvers = {
 
 
         addDay: async (_, {scheduleId, liftId}) =>{
-
-
             try{
                 const schedule = await Schedule.findById(scheduleId);
                 if(!schedule){
